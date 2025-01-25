6d26416ee1474b8bc248c469c74060537438ff13
Giorgio Campisano <campisano.giorgio@gmail.com>
2025-01-25 18:21:59 +0100

    First implementation for avalanches recognition

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ BETA = 0.1  # Weight for degree influence
 GAMMA = 1  # Sensitivity for profit acceptance
 ETA = 0.01  # Scaling factor for price changes
 EXPONENTIAL_SCALING = 1.1 # Exponential scaling factor for high volume differences
-TIME_STEPS = 1000  # Number of time steps for the simulation
+TIME_STEPS = 10000  # Number of time steps for the simulation
+N_RUNS = 10 # Number of repetitions of the experiment
 
 # Initialize a scale-free network using Barabási-Albert model
 G = nx.barabasi_albert_graph(NUM_NODES, m=NUM_HEDGE_FUNDS)
@@ -42,18 +43,13 @@ for idx, (node, degree) in enumerate(sorted_nodes):
 price = 1000
 prices = [price]
 random_lags = np.random.randint(1, 5, size=TIME_STEPS)
-hf_switches_sum = []
-traders_switches_sum = []
 
 # Function to update positions
 def update_positions(t):
     global price
     global random_lags
-    global hf_swithces_sum, traders_swithces_sum
     #Calculate max degree without hedge funds
     max_degree = max([d for n, d in G.degree if G.nodes[n]['type'] != 'hedge_fund'])
-    hf_swithces = 0
-    traders_swithces = 0
 
     for node in G.nodes:
         neighbors = list(G.neighbors(node))
@@ -63,7 +59,6 @@ def update_positions(t):
             profit = np.random.uniform(0, 1)  # Random profit for demonstration
             if np.random.rand() < 1 / (1 + np.exp(-GAMMA * (profit - G.nodes[node]['profit_threshold']))):
                 G.nodes[node]['position'] = 'buy' if G.nodes[node]['position'] == 'sell' else 'sell'
-                hf_swithces += 1 if G.nodes[node]['position'] == 'buy' else -1
                 G.nodes[node]['last_update_time'] = t
                 G.nodes[node]['trade_size'] = np.random.uniform(10, 50)  # Random trade size
         
@@ -77,12 +72,8 @@ def update_positions(t):
             if np.random.rand() < avg_influence:
                 influential_neighbor = max(neighbors, key=lambda n: G.degree[n])
                 G.nodes[node]['position'] = G.nodes[influential_neighbor]['position']
-                traders_swithces += 1 if G.nodes[node]['position'] == 'buy' else -1
                 G.nodes[node]['last_update_time'] = t
                 G.nodes[node]['trade_size'] = np.random.uniform(0.2, 1)  # Random trade size
-
-    hf_switches_sum.append(hf_swithces)
-    traders_switches_sum.append(traders_swithces)
 
 # Function to update market price
 def update_price():
@@ -108,45 +99,57 @@ def update_price():
 
 weighted_volumes = np.zeros(TIME_STEPS)
 
-# Run the simulation
-for t in tqdm(range(TIME_STEPS), desc="Time Step"):
-    update_positions(t)
-    update_price()
-    weighted_volumes[t] = calculate_volume_imbalance(G)
+# Variables to store avalanche information for all the runs
+all_times = []
+all_sizes = []
 
-# Compute the moving average of the market price
-moving_avg = np.convolve(prices, np.ones(25) / 25, mode='valid')
-print("Market volatility: ", np.std(prices))
+print(f"Running {N_RUNS} runs")
+for _ in range(N_RUNS):
+    # Run the simulation
+    for t in tqdm(range(TIME_STEPS), desc="Time step"):
+        update_positions(t)
+        update_price()
+        weighted_volumes[t] = calculate_volume_imbalance(G)
 
-returns = calculate_price_returns(prices)
-print("The mean of returns is: ", np.mean(returns))
-plot_returns(returns, saveFig=False)
+    # Compute the moving average of the market price
+    moving_avg = np.convolve(prices, np.ones(10) / 10, mode='valid')
+    print("Market volatility: ", np.std(prices))
 
-# Added for the 'profile view' in the plot. If in the future we remove it, change the method parameter and remove this.
-plt.figure(figsize=(15, 6))
-plot_market_price(prices, moving_avg, profiler_view=True, saveFig=False)
-ratio = [num_buyers[i] / (num_sellers[i] + num_buyers[i]) for i in range(len(num_buyers))]
-plot_ratio_buyers_sellers(ratio, profiler_view=True, saveFig=False)
-plot_weighted_volumes(weighted_volumes, profiler_view=True, saveFig=False)
+    returns = calculate_price_returns(prices)
+    print("The mean of returns is: ", np.mean(returns))
+    # plot_returns(returns, saveFig=False)
 
-plt.figure(figsize=(15, 6))
-plt.hist(hf_switches_sum + traders_switches_sum, bins=100)
-plt.xlabel('Number of Switches - sum')
-plt.ylabel('Frequency')
-plt.show()
+    # Added for the 'profile view' in the plot. If in the future we remove it, change the method parameter and remove this.
+    # plt.figure(figsize=(15, 6))
+    # plot_market_price(prices, moving_avg, profiler_view=True, saveFig=False)
+    # ratio = [num_buyers[i] / (num_sellers[i] + num_buyers[i]) for i in range(len(num_buyers))]
+    # plot_ratio_buyers_sellers(ratio, profiler_view=True, saveFig=False)
+    # plot_weighted_volumes(weighted_volumes, profiler_view=True, saveFig=False)
 
-plt.figure(figsize=(15, 6))
-plt.hist(hf_switches_sum, bins=100)
-plt.xlabel('Number of Switches (HF)')
-plt.ylabel('Frequency')
-plt.show()
+    starts, ends, times = detect_avalanche(moving_avg, threshold_start=0.0015, threshold_end=0.001)
+    # print("Number of avalanches: ", len(starts))
+    # print("avalanches ends: ", ends)
 
-plt.figure(figsize=(15, 6))
-plt.hist(traders_switches_sum, bins=100)
-plt.xlabel('Number of Switches (Traders)')
-plt.ylabel('Frequency')
-plt.show()
+    price_starts = [prices[i] for i in starts]
+    price_ends = [prices[i] for i in ends]
+    price_diff = [np.abs(price_ends[i] - price_starts[i]) for i in range(len(price_starts))]
 
+    all_times.extend(times)
+    all_sizes.extend(price_diff)
 
+# plt.figure(figsize=(15, 6))
+# plt.plot(prices, label='Market Price', color='blue')
+# plt.plot(moving_avg, label='Moving Average', color='red')
+    
+# for i in range(len(starts)):
+#     plt.vlines(starts[i], 800, 1200, colors='red', linestyles='dashed', linewidth=0.5)
 
+# for i in range(len(ends)):
+#     plt.vlines(ends[i], 800, 1200, colors='green', linestyles='dashed', linewidth=0.5)
+# plt.xlabel('Time Steps')
+# plt.ylabel('Market Price')
+# plt.title('Market Price Evolution')
+# plt.legend()
+# plt.show()
 
+plot_avalanches(all_times, all_sizes)
--- market_statistics.py
+++ market_statistics.py
@@ -1,5 +1,5 @@
 
-__all_ = ['calculate_price_returns', 'calculate_volume_imbalance']
+__all_ = ['calculate_price_returns', 'calculate_volume_imbalance', 'detect_avalanches']
 
 def calculate_price_returns(prices):
     returns = []
@@ -15,3 +15,33 @@ def calculate_volume_imbalance(G):
     sell_volume = sum(G.nodes[node]['trade_size'] for node in G.nodes if G.nodes[node]['position'] == 'sell')
 
     return buy_volume / (sell_volume + buy_volume)
+
+def detect_avalanche(moving_avg, threshold_start=0.0015, threshold_end=0.01):
+    avalanche_starts = []
+    avalanche_ends = []
+    avalanche_times = []
+    i = 1
+    
+    while i < len(moving_avg) - 1:
+        # Check for avalanche start
+        change = (moving_avg[i + 1] - moving_avg[i]) / moving_avg[i]
+        
+        if abs(change) > threshold_start:
+            # Mark avalanche start
+            avalanche_starts.append(i)
+            
+            # Find avalanche end
+            for j in range(i + 1, len(moving_avg) - 1):
+                end_change = (moving_avg[j + 1] - moving_avg[j]) / moving_avg[j]
+                
+                if abs(end_change) < threshold_end:
+                    avalanche_ends.append(j)
+                    avalanche_times.append(j - i)
+                    # Move index to the end of this avalanche
+                    i = j
+                    break
+        
+        # Increment to next time step
+        i += 1
+    
+    return avalanche_starts, avalanche_ends, avalanche_times
